cc0bd7f82264337e4913dd14078e40ed2bfb774b
ncarll <ncarll@users.noreply.github.com>
2019-10-27 20:45:17 -0400

    Initial commit

---
+++ src/test/java/com/enrsx/reactivemicroservice/ReactiveMicroserviceTests.java
@@ -0,0 +1,13 @@
+package com.enrsx.reactivemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMicroserviceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
